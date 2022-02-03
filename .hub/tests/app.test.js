4f554b3a36297001720f6a78a2f7d59eda4d693f
Mateus De Nardo <mateusdnm@hotmail.com>
2022-02-03 17:28:35 -0300

    Update app.test.js

--- .hub/tests/app.test.js
+++ .hub/tests/app.test.js
@@ -2,7 +2,7 @@ import { _setup } from "../utils/serverRunner";
 import fs from "fs";
 
 describe("Usage of schema middleware", () => {
-  it("should be using routers", () => {
+  it("should be using schema middleware", () => {
     const schemaValidationMiddleware = fs.readFileSync("./src/middlewares/userSchemaValidationMiddleware.js", "utf8");
 
     expect(schemaValidationMiddleware.length).toBeGreaterThan(1);
